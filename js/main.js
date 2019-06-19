941b70f23ef0a313a081cf2a77477769af9c92f8
Aznaur <aznaur.khapchaev@gmail.com>
2019-06-19 18:07:23 +0300

    Сгенерировал данные

---
+++ js/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function randomInteger(min, max) {
+    var rand = Math.floor(min + Math.random() * (max - min + 1));
+    rand = Math.round(rand);
+    return rand;
+  }
+
+var messages = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+var authorNames = ['Артем','Алексей','Василий','Аня','Настя','Коля'];
+
+var comments = [];
+for (var i = 0; i < 6; i++) {
+    comments.push({
+        avatar: 'mig/avatar-' + (i + 1) + '.svg',
+        message: messages[i],
+        name: authorNames[i]
+    });
+}
+
+var publishedPhotos = [];
+
+var getArray = function () {
+    
+    for (var j = 0; j < 25; j++) {
+        publishedPhotos.push({
+            url: 'photos/' + (j + 1) + '.jpg',
+            likes: randomInteger(15, 200),
+            comment: comments[randomInteger(0, comments.length - 1)]
+        });
+    }
+};
